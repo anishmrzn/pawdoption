057c89693ce9e363890590e51633d31fe912f5f4
Astha Bajracharya <077bct011.astha@sagarmatha.edu.np>
2024-01-27 02:09:51 +0545

    Setup for minor project

--- Pawdoption/settings.py
+++ Pawdoption/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pawdoption',
         'USER':'root',
-        'PASSWORD':'Or@ngePotion0',
+        'PASSWORD':'reruru12',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
