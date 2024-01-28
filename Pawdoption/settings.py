0ec31058d5d3a341404c855b519c3cbd831a077c
anuragj614 <anuragj614@gmail.com>
2024-01-28 19:33:08 +0545

    create-product achieved

--- Pawdoption/settings.py
+++ Pawdoption/settings.py
@@ -144,15 +144,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL= '/static/'
-MEDIA_URL = '/static/'
-
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
