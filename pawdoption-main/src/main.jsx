b4bd9f81308f2d109aa3dfb880e8f0112e39b7e5
Anish <anishmrzan@gmail.com>
2024-01-30 07:59:38 +0545

    search and sort by category

--- pawdoption-main/src/main.jsx
+++ pawdoption-main/src/main.jsx
@@ -5,14 +5,17 @@ import "./index.css";
 import { ToastContainer } from "react-toastify";
 // import LoginContextProvider from "./context/LoginContextProvider.jsx";
 import { AppProvider } from "./context/ProductContext.jsx";
+import { FilterContextProvider } from "./context/filterContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
-      {/* <LoginContextProvider> */}
-      <ToastContainer />
-      <App />
-      {/* </LoginContextProvider> */}
+      <FilterContextProvider>
+        {/* <LoginContextProvider> */}
+        <ToastContainer />
+        <App />
+        {/* </LoginContextProvider> */}
+      </FilterContextProvider>
     </AppProvider>
   </React.StrictMode>
 );
