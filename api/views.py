834076b7a2444cd62afa094b3bf5606e58533300
Banish Neupane <banishrox@gmail.com>
2024-02-12 22:22:52 +0545

    22:22

--- api/views.py
+++ api/views.py
@@ -10,8 +10,18 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
 from .email_utils import send
 from .models import CustomUser
+from supplierdata.models import Products
+import cloudinary.uploader
 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import joblib
+import pandas as pd
+import json
 
+# Load the trained model
+# loaded_model = joblib.load('dog_breed_classifier_model.joblib')
 
 
 
@@ -31,27 +41,6 @@ def getRoutes(request):
 ]
 
     return Response(routes)
-
-
-@api_view(['GET'])
-def getPets(request):
-  pets = Pets.objects.all()
-  serializer = PetSerializer(pets, many = True)
-  return Response(serializer.data)
-  
-@api_view(['GET'])
-def getSinglePets(request,pk):
-    pets = Pets.objects.get(id = pk)
-    serializer = PetSerializer(pets, many = False)
-    return Response(serializer.data)  
-
-
-@api_view(['GET'])
-def userProfile(request, pk):
-    users = CustomUser.objects.get(id = pk)
-    serializer = UserProfileSerializer(users, many = False)
-    return Response(serializer.data)
-
 
 
 @api_view(['POST'])
@@ -99,35 +88,94 @@ def send_email(request):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-        
+@csrf_exempt
+@api_view(['POST'])
+def predict_dog_breed(request):
+    # print(request.body)
+    try:
+        # Get user responses from the request
+        data = json.loads(request.body)
+        user_responses = data.get('user_responses', [])
+
+        # Convert user responses to DataFrame
+        user_df = pd.DataFrame(user_responses)
+        # user_df = pd.get_dummies(user_df)
+        # print(user_responses)
+        # print(user_df.columns.to_list())
+        print(user_df)
+        # Predict the dog breed
+        prediction = loaded_model.predict(user_df)
+        print("----------------------------------",prediction)
+        recommended_breed = prediction[0]
+
+        return Response({"recommended_breed": recommended_breed})
+
+    except Exception as e:
+        return Response({"error": str(e)})     
+
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
         
 
 import stripe
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+# class StripeCheckoutView(APIView):
+#     def post(self, request):
+#         try:
+#             checkout_session = stripe.checkout.Session.create(
+#                 line_items=[
+#                     {
+#                         'price': 'price_1Oie9XHRxrEnXWBtq8aqWoSX',
+#                         'quantity': 1,
+#                     },
+#                 ],
+#                 payment_method_types=['card',],
+#                 mode='payment',
+#                 success_url=settings.SITE_URL + '/?success=true&session_id={CHECKOUT_SESSION_ID}',
+#                 cancel_url=settings.SITE_URL + '/?canceled=true',
+#             )
+
+#             return redirect(checkout_session.url)
+#         except:
+#             return Response(
+#                 {'error': 'Something went wrong when creating stripe checkout session'},
+#                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
+#             )
+
+
 class StripeCheckoutView(APIView):
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
+        prod_id=Products.productId
         try:
+            product=Products.objects.get(id=prod_id)
             checkout_session = stripe.checkout.Session.create(
                 line_items=[
                     {
-                        'price': 'price_1Oie9XHRxrEnXWBtq8aqWoSX',
+                        # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                        'price_data': {
+                            'currency':'usd',
+                             'unit_amount':int(Products.price) * 100,
+                             'product_data':{
+                                 'name':Products.productName,
+                                 
+
+                             }
+                        },
                         'quantity': 1,
                     },
                 ],
-                payment_method_types=['card',],
+                metadata={
+                    "product_id":Products.productId
+                },
                 mode='payment',
-                success_url=settings.SITE_URL + '/?success=true&session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=settings.SITE_URL + '/?canceled=true',
+                success_url=settings.SITE_URL + '?success=true',
+                cancel_url=settings.SITE_URL + '?canceled=true',
             )
-
             return redirect(checkout_session.url)
-        except:
-            return Response(
-                {'error': 'Something went wrong when creating stripe checkout session'},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
-
+        except Exception as e:
+            return Response({'msg':'something went wrong while creating stripe session','error':str(e)}, status=500)
+        
