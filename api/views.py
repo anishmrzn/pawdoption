8e2e4c66a08e41ca9e7b90735cc87986a4dfe5bc
Banish Neupane <banishrox@gmail.com>
2024-02-13 11:54:44 +0545

    stripe payment 2nd

--- api/views.py
+++ api/views.py
@@ -171,29 +171,6 @@ from django.shortcuts import get_object_or_404
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-# class StripeCheckoutView(APIView):
-#     def post(self, request):
-#         try:
-#             checkout_session = stripe.checkout.Session.create(
-#                 line_items=[
-#                     {
-#                         'price': 'price_1Oie9XHRxrEnXWBtq8aqWoSX',
-#                         'quantity': 1,
-#                     },
-#                 ],
-#                 payment_method_types=['card',],
-#                 mode='payment',
-#                 success_url=settings.SITE_URL + '/?success=true&session_id={CHECKOUT_SESSION_ID}',
-#                 cancel_url=settings.SITE_URL + '/?canceled=true',
-#             )
-
-#             return redirect(checkout_session.url)
-#         except:
-#             return Response(
-#                 {'error': 'Something went wrong when creating stripe checkout session'},
-#                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
-#             )
-
 
 
 
@@ -207,7 +184,7 @@ class StripeCheckoutView(APIView):
 
             products = Products.objects.filter(productId=pk)
 
-
+           
             line_items = []
             for product in products:
                 line_items.append({
@@ -221,6 +198,7 @@ class StripeCheckoutView(APIView):
 
                     'quantity': 1,
                 })
+                
             # for item in line_items:
             #     price_data = item.get('price_data', {})
             #     unit_amount = price_data.get('unit_amount', 0)
@@ -235,7 +213,7 @@ class StripeCheckoutView(APIView):
                 success_url=settings.SITE_URL + '?success=true',
                 cancel_url=settings.SITE_URL + '?canceled=true',
             )
-            print(session)
+            # print(session)
             
             return Response({'message': 'Checkout session created successfully'})
         except Exception as e:
