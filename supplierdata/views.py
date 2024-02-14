6cf81499464ee546d9dce8f1c0ae1f491b31c6de
anuragj614 <anuragj614@gmail.com>
2024-02-14 16:21:07 +0545

    orders table

--- supplierdata/views.py
+++ supplierdata/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProductsSerializer
-from .models import Products
+from .serializers import ProductsSerializer, OrderSerializer
+from .models import Products,Orders
 from rest_framework.permissions import IsAuthenticated
 import cloudinary.uploader
 
@@ -116,6 +116,18 @@ def products(request, pk=None):
     return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-
-
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def orders(request):
+  try:
+    user_id = request.user.id
+    orders = Orders.objects.filter(user = user_id)
+    
+    serializer = OrderSerializer(orders, many = True)
+    
+    return Response(serializer.data, status= status.HTTP_200_OK)
   
+  except:
+    return Response(serializer.errors, status= status.HTTP_404_NOT_FOUND)
+
+ 
