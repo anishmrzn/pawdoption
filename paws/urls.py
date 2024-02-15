f664514c5ca1712ea74578e819e6884d29d9f755
anuragj614 <anuragj614@gmail.com>
2024-02-15 16:02:23 +0545

    animal field in pets

--- paws/urls.py
+++ paws/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import addPets, getPets,send_approval_rejection_emails
+from .views import addPets, getPets,send_feedback_email #send_approval_rejection_emails,
 
 
 
@@ -7,5 +7,6 @@ urlpatterns = [
   path('add-pets/', addPets, name= 'add_pets'),
   path('get-pets/', getPets, name= 'get_pets'),
   path('get-pets/<str:pk>/', getPets, name= 'get_single_pet'),
-  path('email/', send_approval_rejection_emails, name = 'Send_Email_To_Users')
+  # path('email/', send_approval_rejection_emails, name = 'Send_Email_To_Users'),
+  path('email-us/', send_feedback_email, name= "send_feedback_email"),
 ]
--- supplierdata/models.py
+++ supplierdata/models.py
@@ -53,7 +53,7 @@ class Orders(models.Model):
   total_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
   quantity = models.PositiveIntegerField()
   delivery_status = models.CharField(max_length = 10, choices = DELIVERY, default = 'pending')
-  created = models.DateTimeField(auto_now_add = True)
+  created = models.DateField(auto_now_add = True)
   user = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
     
   def __str__(self):
