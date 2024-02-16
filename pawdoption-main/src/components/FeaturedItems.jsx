58b21cf6d88729d4ea6717ac4e0b5d0ede1a47a1
Anish <anishmrzan@gmail.com>
2024-02-16 19:42:41 +0545

    feedback email

--- pawdoption-main/src/components/FeaturedItems.jsx
+++ pawdoption-main/src/components/FeaturedItems.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 import { useProductContext } from "../context/ProductContext";
 import ProductContainer from "./ProductContainer";
+import { useEffect } from "react";
 
-function FeaturedItems({}) {
+function FeaturedItems() {
   const { featureProducts } = useProductContext();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className="flex flex-col items-center gap-10 " data-aos="fade-right">
--- pawdoption-main/src/pages/AboutUs.jsx
+++ pawdoption-main/src/pages/AboutUs.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PageNav from "../components/PageNav";
+import Footer from "../components/Footer";
 
 function AboutUs() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="bg-gray-100">
+    <div>
       <PageNav />
+
       <section className="text-center mt-16">
         <h1 className="text-5xl font-extrabold mb-5 text-[#c9a687]">
           Welcome to Pawdoption
@@ -15,31 +21,147 @@ function AboutUs() {
         </p>
       </section>
 
-      <section className="mt-16 p-8 md:p-16 bg-white rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold mb-8 text-[#c9a687]">Our Mission</h2>
-        <p className="text-lg text-gray-700">
-          At Pawdoption, our mission is to create a world where every pet finds
-          a loving home. We believe in the power of the human-animal bond and
-          strive to make the adoption process a joyful and fulfilling experience
-          for both pets and their new families.
-        </p>
-      </section>
-
-      <section className="mt-16">
-        <h2 className="text-3xl font-bold mb-8 text-[#c9a687]">Our Location</h2>
-
-        <div className="rounded-lg overflow-hidden shadow-lg">
-          <iframe
-            title="Our Location Map"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1766.6494992605492!2d85.31719333438105!3d27.677152231952658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1707987136734!5m2!1sen!2snp"
-            width="100%"
-            height="400"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-          ></iframe>
+      <section className="mt-16 flex flex-col lg:flex-row items-center justify-center gap-16">
+        <div className="w-full md:w-1/2">
+          <div className="rounded-lg overflow-hidden shadow-lg bg-white p-6">
+            <h2 className="text-3xl font-bold mb-4 text-[#c9a687]">
+              Contact Us
+            </h2>
+            <form
+              action="https://formspree.io/f/xkndrvgy"
+              method="POST"
+              className="flex flex-col gap-4"
+            >
+              <input
+                type="text"
+                placeholder="Your Name"
+                name="username"
+                required
+                autoComplete="off"
+                className="border-2 p-2 focus:outline-none rounded-md"
+              />
+              <input
+                type="email"
+                placeholder="Your Email"
+                name="email"
+                required
+                autoComplete="off"
+                className="border-2 p-2 focus:outline-none rounded-md"
+              />
+              <textarea
+                name="message"
+                cols="30"
+                rows="6"
+                required
+                autoComplete="off"
+                placeholder="Enter your message"
+                className="border-2 p-2 focus:outline-none resize-none rounded-md"
+              ></textarea>
+              <button
+                type="submit"
+                className="bg-[#c9a687] text-white py-2 px-4 rounded-md transition duration-300 hover:bg-[#a3825d]"
+              >
+                SEND
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2">
+          <div className="rounded-lg overflow-hidden shadow-lg">
+            <iframe
+              title="Our Location Map"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1766.6494992605492!2d85.31719333438105!3d27.677152231952658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1707987136734!5m2!1sen!2snp"
+              width="100%"
+              height="400"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
       </section>
+
+      {/* Photo Gallery Section */}
+      <section className="mt-16 p-8 md:p-16 bg-[#f0f0f0] rounded-lg shadow-lg text-center">
+        <h2 className="text-3xl font-bold mb-8 text-[#c9a687]">
+          Our Pet Adoption Gallery
+        </h2>
+
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-1">
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery.webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500 "
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (2).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (3).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (4).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (5).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (6).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (7).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (8).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (9).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <img
+              src="/petgallery (10).webp"
+              alt="Pet Adoption 3"
+              className="w-[20rem] h-[10rem] hover:scale-[120%] transition-all duration-500"
+            />
+          </div>
+        </div>
+      </section>
+
+      <Footer />
     </div>
   );
 }
