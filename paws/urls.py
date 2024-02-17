d6811efb34db6fda2030d145c5386ec23c8ba899
anuragj614 <anuragj614@gmail.com>
2024-02-17 17:53:41 +0545

    change password

--- paws/urls.py
+++ paws/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import addPets, getPets,send_feedback_email #send_approval_rejection_emails,
+from .views import addPets, getPets, ChangePassword #send_approval_rejection_emails,
 
 
 
@@ -7,6 +7,6 @@ urlpatterns = [
   path('add-pets/', addPets, name= 'add_pets'),
   path('get-pets/', getPets, name= 'get_pets'),
   path('get-pets/<str:pk>/', getPets, name= 'get_single_pet'),
+  path('password/',ChangePassword , name= 'Change_Password'),
   # path('email/', send_approval_rejection_emails, name = 'Send_Email_To_Users'),
-  path('email-us/', send_feedback_email, name= "send_feedback_email"),
 ]
