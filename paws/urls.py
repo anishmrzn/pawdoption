62f22ea52d934bd1b7e1a4c8819af3205739a4aa
Banish Neupane <banishrox@gmail.com>
2024-02-18 10:19:59 +0545

    petadoptionApi

--- paws/urls.py
+++ paws/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import addPets, getPets,send_feedback_email #send_approval_rejection_emails,
+from .views import addPets, getPets,send_feedback_email,submit_adoption_application #send_approval_rejection_emails,
 
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
   path('get-pets/<str:pk>/', getPets, name= 'get_single_pet'),
   # path('email/', send_approval_rejection_emails, name = 'Send_Email_To_Users'),
   path('email-us/', send_feedback_email, name= "send_feedback_email"),
+  path('petadoption-form/',submit_adoption_application, name= "submit_adoption_application")
 ]
--- paws/views.py
+++ paws/views.py
@@ -6,7 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializers import PetSerializer
+from .serializers import PetSerializer, PetAdoptionSerializer
+
 from .models import Pets
 
 # Create your views here.
@@ -122,6 +123,19 @@ def send_feedback_email(request):
         return Response({'error': 'Only POST requests are allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def submit_adoption_application(request):
+    if request.method == 'POST':
+        serializer = PetAdoptionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    else:
+      return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
     
   
   
